69b5a4e822e7de349ae3e3c989a4a872036ec113
Gabriel Nunes <gabriel.nunesantoscoelho@gmail.com>
2026-05-08 16:03:51 -0300

    chore: configura husky e commitlint

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,12 @@ const App = () => {
   return (
     <>
       <h1>App</h1>
+      <h1>App</h1>
+      <h1>App</h1>
+      <h1>App</h1>
+      <h1>App</h1>
     </>
-  );
-};
+  )
+}
 
-export default App;
+export default App
