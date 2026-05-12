804f33a967094b5d1cd8f0a954f2b1c0ea09efca
Gabriel Nunes <gabriel.nunesantoscoelho@gmail.com>
2026-05-12 17:46:42 -0300

    feat: add initial login page

--- src/pages/loginPage.jsx
+++ src/pages/loginPage.jsx
@@ -1,8 +1,40 @@
+import { Link } from 'react-router'
+
+import PasswordInput from '@/components/passwordInput'
+import { Button } from '@/components/ui/button'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
+
 const LoginPage = () => {
   return (
-    <>
-      <h1>Login Page</h1>
-    </>
+    <div className="flex h-screen w-screen flex-col items-center justify-center gap-3">
+      <Card className="w-125">
+        <CardHeader className="text-center">
+          <CardTitle>Entre na sua conta</CardTitle>
+          <CardDescription>Insira os seus dados abaixo:</CardDescription>
+        </CardHeader>
+        <CardContent className="space-y-4">
+          <Input placeholder="Digite seu E-mail" />
+          <PasswordInput placeholder="Digite sua senha" />
+        </CardContent>
+        <CardFooter>
+          <Button className="w-full">Fazer login</Button>
+        </CardFooter>
+      </Card>
+      <div className="flex items-center justify-center">
+        <p className="text-center opacity-50">Ainda não possui uma conta?</p>
+        <Button variant="link" asChild>
+          <Link to="/signup">Crie agora</Link>
+        </Button>
+      </div>
+    </div>
   )
 }
 
